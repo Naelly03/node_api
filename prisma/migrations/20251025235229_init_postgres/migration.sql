d633e92d1b9e5e865e19073a2fc769c04dfdfa1b
Naelly03 <naellyvitoria8@gmail.com>
2025-10-25 20:53:53 -0300

    feat: corrigir migrações para PostgreSQL

---
+++ prisma/migrations/20251025235229_init_postgres/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "Curso" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+
+    CONSTRAINT "Curso_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Aluno" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "Aluno_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Matricula" (
+    "id" SERIAL NOT NULL,
+    "cursoId" INTEGER NOT NULL,
+    "alunoId" INTEGER NOT NULL,
+
+    CONSTRAINT "Matricula_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Aluno_email_key" ON "Aluno"("email");
+
+-- AddForeignKey
+ALTER TABLE "Matricula" ADD CONSTRAINT "Matricula_cursoId_fkey" FOREIGN KEY ("cursoId") REFERENCES "Curso"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Matricula" ADD CONSTRAINT "Matricula_alunoId_fkey" FOREIGN KEY ("alunoId") REFERENCES "Aluno"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
